60f43831655fb3308a505642307111510095768d
GlobusShwobus <fjodorzaro@gmail.com>
2025-07-24 23:22:42 +0300

    iterator. not blowing up my leg today, maybe in two weeks tho

---
+++ recomp/SeqIterator.h
@@ -0,0 +1,209 @@
+#pragma once
+
+#include <cstddef>
+#include <iterator>
+
+namespace badEngine {
+
+	template <typename T>
+	class ConstSeqIterator {
+	public:
+		using self_type = ConstSeqIterator;
+		using value_type = T;
+		using reference = const T&;
+		using pointer = const T*;
+		using difference_type = std::ptrdiff_t;
+		using iterator_category = std::random_access_iterator_tag;
+
+		ConstSeqIterator() = default;
+		ConstSeqIterator(pointer value) :value(value) {}
+		ConstSeqIterator(const SeqIterator<T>& iter) :value(&*iter) {}
+
+		reference operator*()const {
+			return *value;
+		}
+		pointer operator->()const {
+			return value;
+		}
+
+		self_type& operator++() {
+			++value;
+			return *this;
+		}
+		self_type operator++(int) {
+			self_type temp = *this;
+			++value;
+			return temp;
+		}
+		self_type& operator--() {
+			--value;
+			return *this;
+		}
+		self_type operator--(int) {
+			self_type temp = *this;
+			--value;
+			return temp;
+		}
+
+		self_type& operator+=(difference_type n) {
+			value += n;
+			return *this;
+		}
+		self_type& operator-=(difference_type n) {
+			value -= n;
+			return *this;
+		}
+
+		self_type operator+(difference_type n)const {
+			return self_type(value + n);
+		}
+		self_type operator-(difference_type n)const {
+			return self_type(value - n);
+		}
+		difference_type operator-(const self_type& rhs)const {
+			return value - rhs.value;
+		}
+
+		reference operator[](difference_type n)const {
+			return value[n];
+		}
+
+		bool operator==(const self_type& rhs)const {
+			return value == rhs.value;
+		}
+		bool operator!=(const self_type& rhs)const {
+			return value != rhs.value;
+		}
+		bool operator<(const self_type& rhs)const {
+			return value < rhs.value;
+		}
+		bool operator>(const self_type& rhs)const {
+			return value > rhs.value;
+		}
+		bool operator<=(const self_type& rhs)const {
+			return value <= rhs.value;
+		}
+		bool operator>=(const self_type& rhs)const {
+			return value >= rhs.value;
+		}
+
+	private:
+		const T* value = nullptr;
+	};
+
+	template<typename T>
+	class SeqIterator {
+	public:
+		using self_type = SeqIterator;
+		using value_type = T;
+		using reference = T&;
+		using pointer = T*;
+		using difference_type = std::ptrdiff_t;
+		using iterator_category = std::random_access_iterator_tag;
+
+		SeqIterator() = default;
+		SeqIterator(pointer value) :value(value) {}
+
+		const reference operator*()const {
+			return *value;
+		}
+		reference operator*() {
+			return *value;
+		}
+		const pointer operator->()const {
+			return value;
+		}
+		pointer operator->() {
+			return value;
+		}
+
+		self_type& operator++() {
+			++value;
+			return *this;
+		}
+		self_type operator++(int) {
+			self_type temp = *this;
+			++value;
+			return temp;
+		}
+
+		self_type& operator--() {
+			--value;
+			return *this;
+		}
+		self_type operator--(int) {
+			self_type temp = *this;
+			--value;
+			return temp;
+		}
+
+		self_type& operator+=(difference_type n) {
+			value += n;
+			return *this;
+		}
+		self_type& operator-=(difference_type n) {
+			value -= n;
+			return *this;
+		}
+
+		self_type operator+(difference_type n)const {
+			return self_type(value + n);
+		}
+		self_type operator-(difference_type n)const {
+			return self_type(value - n);
+		}
+		difference_type operator-(const self_type& rhs) {
+			return value - rhs.value;
+		}
+
+		const reference operator[](difference_type n)const {
+			return value[n];
+		}
+		reference operator[](difference_type n ) {
+			return value[n];
+		}
+
+		bool operator==(const self_type& rhs)const {
+			return value == rhs.value;
+		}
+		bool operator!=(const self_type& rhs)const {
+			return value != rhs.value;
+		}
+		bool operator<(const self_type& rhs)const {
+			return value < rhs.value;
+		}
+		bool operator>(const self_type& rhs)const {
+			return value > rhs.value;
+		}
+		bool operator<=(const self_type& rhs)const {
+			return value <= rhs.value;
+		}
+		bool operator>=(const self_type& rhs)const {
+			return value >= rhs.value;
+		}
+	private:
+		friend class ConstSeqIterator;
+		T* value = nullptr;
+	};
+
+	template <typename T>
+	SeqIterator<T> operator+(typename SeqIterator<T>::difference_type n, const SeqIterator<T>& it) {
+		return it + n;
+	}
+	template<typename T>
+	ConstSeqIterator<T> operator+(typename ConstSeqIterator<T>::difference_type n, const ConstSeqIterator<T>& it) {
+		return it + n;
+	}
+
+
+	/*
+
+==
+!=
+<
+>
+<=
+>=
+*/
+
+}
--- recomp/Sequence.h
+++ recomp/Sequence.h
@@ -1,52 +1,103 @@
 #pragma once
 
 #include <utility>
+#include "SeqIterator.h"
 
-template <typename T>
-class Sequence {
+namespace badEngine {
+	template <typename T>
+	class Sequence {
 
-	T* array = nullptr;
-	size_t size;
-	size_t cap;
+		T* array = nullptr;
+		size_t size;
+		size_t cap;
 
-	void grow() {
-		size_t newCap = (cap == 0) ? 1 : cap * 2;//assign new size
-		void* bytes = ::operator new(newCap * sizeof(T));//allocate raw bytes
-		T* newArray = static_cast<T*> bytes;//cast it to the actual type
+		void grow() {
+			size_t newCap = (cap == 0) ? 1 : cap * 2;//assign new size
+			void* bytes = ::operator new(newCap * sizeof(T));//allocate raw bytes
+			T* newArray = static_cast<T*>(bytes);//cast it to the actual type
 
-		for (size_t i = 0; i < size; i++) {
-			new (newArray + i) T(std::move(array[i]));//beginning of the sequence + sizeof(T) implicit, ptr arithmetic in ( )
+			for (size_t i = 0; i < size; i++) {
+				new (newArray + i) T(std::move(array[i]));//beginning of the sequence + sizeof(T) implicit, ptr arithmetic in ( )
+			}
+
+			for (size_t i = 0; i < size; i++) {
+				array[i].~T();
+			}
+
+			::operator delete(array, cap * sizeof(T));//deletes the whole memory sequence bytes thing, does not care about the data there, meaning does call their destructors
+			array = newArray;
+			cap = newCap;
 		}
 
-		for (size_t i = 0; i < size; i++) {
-			array[i].~T();
+	public:
+		Sequence() :array(nullptr), size(0), cap(0) {}
+		~Sequence() {
+			for (size_t i = 0; i < size; ++i) {
+				array[i].~T();//first destroy the element
+			}
+			::operator delete(array, cap * sizeof(T));//then delete the bytes
 		}
 
-		::operator delete(array);//deletes the whole memory sequence bytes thing, does not care about the data there, meaning does call their destructors
-		array = newArray;
-		cap = newCap;
-	}
-
-public:
-	Sequence() :array(nullptr), size(0), cap(0) {}
-	~Sequence() {
-		for (size_t i = 0; i < size; ++i) {
-			array[i].~T();//first delete the element
+		void add(const T& value) {
+			if (size == cap)
+				grow();
+			new(array + size) T(value);//beginning of the sequence+ptr arithmetic index, place the value there
+			++size;
 		}
-		::operator delete(array);//then delete the bytes
-	}
+		void add(T&& value) {
+			if (size == cap)
+				grow();
+			new(array + size) T(std::move(value));//beginning of the sequence+ptr arithmetic index, place the value there
+			++size;
+		}
 
-	void add(const T& value) {
-		if (size == cap)
-			grow();
-		new(array + size) T(value);//beginning of the sequence+ptr arithmetic index, place the value there
-		++size;
-	}
-	void add(T&& value) {
-		if (size == cap)
-			grow();
-		new(array + size) T(std::move(value));//beginning of the sequence+ptr arithmetic index, place the value there
-		++size;
-	}
+		using iterator = SeqIterator<T>;
+		using const_iterator = ConstSeqIterator<T>;
 
-};
+		iterator begin() {
+			return iterator(array);
+		}
+		iterator end() {
+			return iterator(array + size);
+		}
+		const_iterator begin()const {
+			return const_iterator(array);
+		}
+		const_iterator end()const {
+			return const_iterator(array + size);
+		}
+		const_iterator cbegin()const {
+			return const_iterator(array);
+		}
+		const_iterator cend()const {
+			return const_iterator(array + size);
+		}
+
+			/*
+			TODO
+			pop back
+			iterators
+			erase (but not insert in middle, fuck it, never use it)
+			erase range
+			clear
+
+			empty
+			size
+			cap
+			operator []
+
+			resize
+			reserve
+			emplace_back - i have no idea how to even but need to know ( && ? )
+			append range (add multiple in one go, initalizer list?)
+			shrink to fit
+
+
+			rule of 5 constructors
+			initalizer list constructor
+
+			IMPORTNAT:
+				static asserts to make sure object has default, copy and move constructors, otherwise == no bueno
+				*/
+	};
+}
